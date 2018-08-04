aaae2aa93726c7c1bd45d8e79644e050cf5626f1
sudhirmandarapu <sudhirmandarapu@gmail.com>
2018-08-04 14:24:10 +1000

    Added upload image endpoint

--- backend/images.py
+++ backend/images.py
@@ -3,27 +3,27 @@ import os
 import hashlib
 import time
 
-UPLOAD_FOLDER = 'backend/uploads'
+UPLOAD_FOLDER = './uploads'
 images_blueprint = Blueprint('images', __name__, template_folder='templates')
 
 
 @images_blueprint.route('/', methods=['POST'])
-def upload_file():
-    print('here')
-    return "Complete"
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(UPLOAD_FOLDER, filename))
-                return redirect(url_for('uploaded_file',
-                                        filename=filename))
+def upload_image():
+    # check if the post request has the file part
+    if 'image' not in request.files:
+        return redirect(request.url)
+    file = request.files['image']
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(UPLOAD_FOLDER, filename))
+        return url_for('images.get_image', filename=filename)
+
+
+@images_blueprint.route('/<filename>', methods=['GET'])
+def get_image(filename):
+    return filename
 
 
 def allowed_file(filename):
@@ -31,6 +31,7 @@ def allowed_file(filename):
 
 
 def secure_filename(filename):
+    _, extension = os.path.splitext(filename)
     name_hash = hashlib.sha1()
-    name_hash.update(str(time.time()) + filename)
-    return name_hash.hexdigest()[:10]
+    name_hash.update((str(time.time()) + str(filename)).encode('utf-8'))
+    return name_hash.hexdigest()[:10]+extension
