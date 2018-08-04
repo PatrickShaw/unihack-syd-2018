2e12dd6c8ecf2ab29d113c6877c01b9811ec889c
Patrick Shaw <psha67@student.monash.edu>
2018-08-04 19:54:43 +1000

    Removed transitions cause they don't work and i cbs :P

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import withStyles from '@material-ui/core/styles/withStyles';
-import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 import { HomePage } from './pages/HomePage';
 import { MapPage } from './pages/MapPage';
@@ -9,7 +8,6 @@ import { VideoPage } from './pages/VideoPage';
 import { ErrorPage } from './pages/ErrorPage';
 import ListPage from './pages/ListPage';
 import { ContentContainer }  from './components/ContentContainer';
-import { CssBaseline } from '../node_modules/@material-ui/core';
 
 export const App = withStyles({
   app: {
@@ -29,29 +27,23 @@ export const App = withStyles({
 })(class App extends Component {
   render() {
     return (
-      <Route render={({ location }) => (
-        <div className={this.props.classes.app}>
-          <ContentContainer>
-            <TransitionGroup>
-              <CSSTransition key={location.key} className={this.props.classes.fade} timeout={200}>
-                <Switch>
-                  <Route path='/map' component={MapPage}/>
-                  <Route path='/list' component={ListPage}/>
-                  <Route path='/cctv' render={() => <VideoPage
-                      videoTitle="Swag"
-                      video={{
-                        url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-                      }}
-                    />}/>
-                  <Route path='/home' component={HomePage}/>
-                  <Route exact path='/' component={HomePage}/>
-                  <Route path='/' component={ErrorPage}/>
-                </Switch>
-              </CSSTransition>
-            </TransitionGroup>
-          </ContentContainer>
-        </div>
-      )}/>
+      <div className={this.props.classes.app}>
+        <ContentContainer>
+          <Switch>
+            <Route path='/map' component={MapPage}/>
+            <Route path='/list' component={ListPage}/>
+            <Route path='/cctv' render={() => <VideoPage
+                videoTitle="Swag"
+                video={{
+                  url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+                }}
+              />}/>
+            <Route path='/home' component={HomePage}/>
+            <Route exact path='/' component={HomePage}/>
+            <Route path='/' component={ErrorPage}/>
+          </Switch>
+        </ContentContainer>
+      </div>
     );
   }
 });
