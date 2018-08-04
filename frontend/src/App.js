b83ab9887a53f04594e0d955e3467ff0a8c6e215
Patrick Shaw <psha67@student.monash.edu>
2018-08-04 17:59:13 +1000

    Merged the things

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import { HomePage } from './pages/HomePage';
 import { MapPage } from './pages/MapPage';
 import { ListPage } from './pages/ListPage';
+import { VideoPage } from './pages/VideoPage';
 import { ContentContainer }  from './components/ContentContainer';
 
 export const App = withStyles({
@@ -20,6 +21,12 @@ export const App = withStyles({
           <Route exact path='/' component={MapPage}/>
           <Route exact path='/home' component={HomePage}/>
           <Route exact path='/List' component={ListPage}/>
+          <Route path='/cctv' render={() => <VideoPage
+              videoTitle="Swag"
+              video={{
+                url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+              }}
+            />}/>
         </ContentContainer>
       </div>
     );
---
+++ frontend/src/pages/VideoPage.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import video from '../vid/rick.mp4';
+
+const LabelThing = ({label, value}) => (
+  value ?   <p><strong>{label}</strong>{value ? ` ${value}` : ''}</p> : null
+)
+
+export const VideoPage = withStyles({
+  video: {
+    maxHeight: '400px'
+  }
+})(class VideoPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.video.title}</h1>
+        <video className={this.props.classes.video} control>
+          <source src={this.props.video.src} type="video/mp4"/>
+        </video>
+        <LabelThing name={'Status'} value={this.props.status}/>
+      </div>
+    );
+  }
+})
