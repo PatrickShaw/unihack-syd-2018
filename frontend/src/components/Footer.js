3a6c1d5889658498f71bd73053947ad6908b6c80
stephaniesac <stephanie.a.chua@gmail.com>
2018-08-04 18:43:36 +1000

    Fixed footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,13 +3,22 @@ import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 
 export const Footer = withStyles({
-
+  footer: {
+    backgroundColor: '#000000',
+    height: '40px',
+    padding: '30px',
+    overflow: 'hidden',
+    fontFamily: 'waukegan',
+    color: '#ffffff',
+    fontSize: '1.7em',
+    textAlign: 'right',
+  }
 })(class Footer extends Component {
   render() {
     return (
-      <div>
-        JUIIICCYY GRAPES!
-      </div>
+      <p className={this.props.classes.footer}>
+        Team JuicyGrapes
+      </p>
     );
   }
 });
