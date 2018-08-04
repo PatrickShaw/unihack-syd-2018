c0d3bbe5ad56c2863f9dddf473fedc8bd04ced7f
Patrick Shaw <psha67@student.monash.edu>
2018-08-04 13:47:43 +1000

    Added more things

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+import { MapPage } from './pages/MapPage';
+import { Route } from 'react-router-dom';
+
+export class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Route exact path='/' component={MapPage}/>
       </div>
     );
   }
 }
-
-export default App;
